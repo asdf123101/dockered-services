35f7b0546107d69b33252e870427d5129d7b450a
asdf123101 <nickeysnowing@gmail.com>
2018-03-24 21:13:48 -0400

    Add nginx reverse proxy server

--- gen_db.sh
+++ gen_db.sh
@@ -4,15 +4,15 @@
 # are the same as the database name
 
 function create_db_if_non {
-	for NAME in "$@"
-		do
-			if [ -z "$(mysql -u $DBROOTNAME -p $DBROOTPWD -e "show databases like '$NAME'")" ]; then
-				echo "create $NAME"
-				mysql -uroot -proot -e "create database $NAME; \
-				GRANT ALL PRIVILEGES ON $NAME.* TO \
-				'$NAME'@'%' IDENTIFIED BY '$NAME'"
-			fi
-		done
+	# remove extra quotes
+	LIST="${1//\"}"
+	for NAME in $LIST
+	do
+		if [ -z "$(mysql -u root -p "$DBROOTPWD" -e "show databases like '$NAME'")" ]; then
+			echo "create $NAME"
+			mysql -uroot -proot -e "create database $NAME; GRANT ALL PRIVILEGES ON $NAME.* TO '$NAME'@'%' IDENTIFIED BY '$NAME'"
+		fi
+	done
 }
 
-create_db_if_non ${GENDBLIST}
+create_db_if_non "${GENDBLIST}"
